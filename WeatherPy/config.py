4ee57b4f2edd3e04d58a6afb89f9863641b75558
mmoormeier <64701512+mmoormeier@users.noreply.github.com>
2020-09-03 09:53:35 -0400

    Delete config.py

--- WeatherPy/config.py
+++
@@ -1,9 +0,0 @@
-# Enter your API key
-
-gkey = "AIzaSyAuY0k57ByTsm1MOGnA_UaN7xqJNFCqM04"
-
-census_key = "d52e1529556a6a29e3b8d855e6122ba7eb8a3826"
-
-omdb_key = "44fbd10d"
-
-weather_key = "194c964e5a1a1183aaa918d387570b66"
